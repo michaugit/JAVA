02ce0d407546f98dcb848afdbdd6ced2a3776cad
Michał <michalkontonpg@gmail.com>
2019-10-03 21:13:01 +0200

    no message

--- LAB_1/src/com/company/DataFrame.java
+++ LAB_1/src/com/company/DataFrame.java
@@ -1,4 +1,56 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class DataFrame {
+    DataFrame( String [] name, String [] type){
+        data= new TreeMap<>();
+        sizeOfDate=0;
+        int iter=0;
+
+        for ( String str : name) {
+            switch(type[iter]){
+                case "int":
+                    data.put(str, new ArrayList<Integer>());
+                    break;
+                case "string":
+                    data.put(str, new ArrayList<String>());
+                    break;
+                default:
+                    System.out.println("Unknown type!");
+                    break;
+            }
+            iter++;
+        }
+
+    }
+    public final int size(){
+        for(Map.Entry<String, ArrayList> pair : data.entrySet() ){
+            if(sizeOfDate<pair.getValue().size()){
+                sizeOfDate=pair.getValue().size();
+            }
+        }
+        return sizeOfDate;
+    }
+
+    public ArrayList get(String colname){
+        return data.get(colname);
+    }
+
+    public DataFrame iloc(int i){
+        String [] columnNames= new String[data.size()];
+        String [] columnTypes= new String[data.size()];
+        int iter=0;
+        for(Map.Entry<String, ArrayList> pair : data.entrySet() ){
+
+            iter++;
+        }
+
+    }
+
+    private Map< String , ArrayList> data;
+    private int sizeOfDate;
+
 }
--- LAB_1/src/com/company/Main.java
+++ LAB_1/src/com/company/Main.java
@@ -4,6 +4,15 @@ import com.company.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HEllo World");
+        DataFrame df =  new DataFrame(new String[] {"kolumna 1", "kolumna 2"}, new String []{"int","string"});
+
+
+
+        df.get("kolumna 1").add(1);
+        df.get("kolumna 1").add(2);
+        df.get("kolumna 1").add(3);
+
+        df.get("kolumna 1").forEach(name -> System.out.println(name));
+        System.out.println("\n Number of rows: " + df.size());
     }
 }
